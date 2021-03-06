44af7bdb3012ffc7e8a4a50256a97e281fd73c16
indriruth13 <indriruth.95@gmail.com>
2021-03-06 17:38:06 +0800

    finished styling and functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @font-face {
+    font-family: "Airbnb Cereal Black";
+    src: local("Airbnb Cereal Black"),
+    url(../src/assets/fonts/AirbnbCerealBlack.ttf) format("truetype");
+  }
+
+  @font-face {
+    font-family: "Airbnb Cereal Light";
+    src: local("Airbnb Cereal Light"),
+    url(../src/assets/fonts/AirbnbCerealLight.ttf) format("truetype");
+  }
+
+  @font-face {
+    font-family: "Airbnb Cereal Book";
+    src: local("Airbnb Cereal Book"),
+    url(../src/assets/fonts/AirbnbCerealBook.ttf) format("truetype");
+  }
+
+  .v-application {
+    font-family: 'Airbnb Cereal Light' !important;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import vuetify from './plugins/vuetify'
+import './style.css'
+import VueDragDrop from 'vue-drag-drop'
+ 
 Vue.config.productionTip = false
+Vue.use(vuetify);
+Vue.use(VueDragDrop);
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
